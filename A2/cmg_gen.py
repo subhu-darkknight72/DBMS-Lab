bbbc02b07602f9752f178cf6eea84eb10d1d03eb
Subhajyoti Halder <subhu@Subhajyotis-MacBook-Air.local>
2023-02-05 16:51:13 +0530

    Create repo

---
+++ A2/cmg_gen.py
@@ -0,0 +1,27 @@
+Names = [
+    "Subhajyoti",
+    "not_Geetanjali",
+    "Pabitra",
+    "Jayanta",
+    "Aurobindo",
+    "Abhijit",
+    "Sudeshna",
+    "KSRao",
+    "Abir",
+    "Debdweepa",
+    "Mainack",
+    "Saptarshi",
+    "Shivansh",
+    "Anubhav"
+]
+
+18094
+17296
+18118
+12705
+17288
+18355
+15173
+19410
+18563
+17254
